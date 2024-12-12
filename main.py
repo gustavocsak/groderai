0fc98a29a34d7a8a6d96068223437cde8a5c03e1
Gustavo <gustavocsak@gmail.com>
2024-12-11 23:53:00 -0800

    feat: integrate gemini

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+import google.generativeai as genai
+
+gemini_key = os.getenv('GEMINI_KEY')
+genai.configure(api_key=gemini_key)
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Explain how AI works")
+print(response.text)
