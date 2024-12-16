cb48fbcba5ccf7b0f7957783882ec2fee9b8e3e5
Gustavo <gustavocsak@gmail.com>
2024-12-16 09:40:18 -0800

    refactor: change prompt and response schema

--- main.py
+++ main.py
@@ -13,37 +13,54 @@ with open("assignment.txt", "r") as f:
 with open("sample2.java", "r") as f:
   code = f.read()
 
+class Metadata(BaseModel):
+  title: str
+  description: str
+  requirements: list[str]
+  language: str
+
 class Method(BaseModel):
   prototype: str
   expected_prototype: str
   time_complexity: Literal["O(1)", "O(n)", "O(n^2)", "O(log n)", "O(n log n)"]
   is_documented: bool
-  indentation: Literal["Consistent", "Inconsistent"]
-  naming_convention: Literal["Consistent", "Inconsistent"]
   errors: List[str]
   is_correct: bool
 
-class Report(BaseModel):
-  methods: List[Method]
-  notes: List[str]
+class CodeAnalysis(BaseModel):
+  readability: list[str]
+  documentation: list[str]
+  linting_errors: list[str]
+  missing_requirements: list[str]
+  restricted_usage: list[str]
+
+class Assignment(BaseModel):
+  metadata: Metadata
+  methods: list[Method]
+  code_analysis: CodeAnalysis
+
 
 
 gemini_key = os.getenv('GEMINI_KEY')
+print(gemini_key)
 genai.configure(api_key=gemini_key)
 model = genai.GenerativeModel(
   model_name="gemini-1.5-flash",
   system_instruction="Act as a coding assistant tool, report any errors and findings according to the assignment instructions given"
 )
 
-prompt = f"""
-  Analyze the following assignment instructions and the provided student code. Evaluate each method in the code based on the instructions and generate a report.
 
-  **Important**:
-  1. Every method in the student code must include all attributes from the schema.
-  2. Ensure the evaluation considers the assignment instructions provided.
-  4. Explain any possible errors or not allowed classes/methods in the `errors` array
-  5. If a method is not described in the assignment instructions, fill the expected_prototype with "N/A"
-  6. Any additional notes go into the notes field (missing requirements, methods), make it very brief, separate into multiple strings
+prompt = f"""
+  You are part of GroderAI, a tool designed to augment the grading process for coding assignments by analyzing and summarizing both the submitted code and the assignment instructions. Your goal is to provide a clear and concise report.
+  ### Task
+  - You will receive a text containing the assignment instructions.
+  - Along with this, you will analyze the code submitted by the student.
+
+  ### Role
+  - Your responsibility is to carefully examine the assignment instructions and the student's code.
+  - Identify and highlight key details and important information.
+  - Present your findings in alignment with the provided response schema.
+  - You must fill all the fields in the report, if something is not applicable use "N/A"
 
   Instructions:
   {instructions}
@@ -56,7 +73,7 @@ prompt = f"""
 result = model.generate_content(
     prompt,
     generation_config=genai.GenerationConfig(
-        response_mime_type="application/json", response_schema=Report
+        response_mime_type="application/json", response_schema=Assignment
     ),
 )
 
